66687a8deb6eeadc0283aef2af07a28a6c163b11
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-05-27 03:24:23 +0530

    - created firebase/auth app with email and password sign in

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { auth } from './assets/config';
 
 function App() {
+  const [regUser, setRegUser] = useState('')
+  const [regPass, setRegPass] = useState('')
+  const [logUser, setLogUser] = useState('')
+  const [logPass, setLogPass] = useState('')
+  const [user, setUser] = useState({})
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser)
+  })
+
+  const register = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        regUser,
+        regPass
+      )
+      console.log(user)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        logUser,
+        logPass
+      )
+      console.log(user)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  const logout = async () => {
+    await signOut(auth)
+  }
+
+  useEffect(() => {
+
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className='register'>
+        <input
+          type='text'
+          placeholder='email@gmail.com'
+          onChange={(e) => {
+            setRegUser(e.target.value)
+          }}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          onChange={(e) => {
+            setRegPass(e.target.value)
+          }}
+        />
+        <input
+          type='submit'
+          value='Register'
+          onClick={(e) => {
+            register()
+          }}
+        />
+      </div>
+      <div className='login'>
+        <input
+          type='text'
+          placeholder='email@gmail.com'
+          onChange={(e) => {
+            setLogUser(e.target.value)
+          }}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          onChange={(e) => {
+            setLogPass(e.target.value)
+          }}
+        />
+        <input
+          type='submit'
+          value='Login'
+          onClick={(e) => {
+            login()
+          }}
+        />
+      </div>
+      <div className='logout'>
+        {user?.email}
+        <button
+          onClick={(e) => {
+            logout()
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
